538736f1f26351710db6fb0665c0d642dd5485ce
Nick <trashemail430@gmail.com>
2021-11-29 17:42:32 -0500

    added rich and cleaned up output

--- plasma-benchmarking.py
+++ plasma-benchmarking.py
@@ -3,12 +3,17 @@
 import csv
 import numpy as np
 import os
-from pyarrow import fs
+import pyarrow as pa
+import pyarrow.parquet as pq
 import pyarrow.plasma as plasma
+from rich import print
+from rich.console import Console
+from rich.progress import BarColumn, Progress
+from rich.table import Column, Table
 import sys
 import timeit
 
-local = fs.LocalFileSystem()
+local = pa.fs.LocalFileSystem()
 
 def random_obj_id() -> plasma.ObjectID:
     '''
@@ -29,6 +34,9 @@ def check_connection(client: plasma.PlasmaClient) -> bool:
 
 
 def roundtrip_file(client: plasma.PlasmaClient, filename: str):
+    '''
+    Roundtrip a native Python file object's bytes to the Plasma store.
+    '''
     obj_id = random_obj_id()
     # replace end of filename w/ _out & change directory from in to out
     out_filename = filename.replace(".", "_out.").replace("in", "out")
@@ -50,6 +58,9 @@ def roundtrip_file(client: plasma.PlasmaClient, filename: str):
 
 
 def roundtrip_file_stream(client: plasma.PlasmaClient, filename: str):
+    '''
+    Roundtrip a PyArrow file stream's bytes to the Plasma store.
+    '''
     global local
 
     obj_id = random_obj_id()
@@ -71,71 +82,25 @@ def roundtrip_file_stream(client: plasma.PlasmaClient, filename: str):
         stream.write(buf)
 
 
-def main():
-    # connect to the running plasma server
-    start = timeit.default_timer()
-    client = plasma.connect("/tmp/plasma")
-    end = timeit.default_timer()  
-
-    # perform all the timeit operations
-    connection_check_time = timeit.timeit(lambda: check_connection(client), number=10)
-
-    csv_file_time  = timeit.timeit(lambda: roundtrip_file(client, "in/test_data.csv"),  number=10)
-    json_file_time = timeit.timeit(lambda: roundtrip_file(client, "in/test_data.json"), number=10)
-  
-    csv_stream_time     = timeit.timeit(lambda: roundtrip_file_stream(client, "in/test_data.csv"),     number=10)
-    json_stream_time    = timeit.timeit(lambda: roundtrip_file_stream(client, "in/test_data.json"),    number=10)
-    parquet_stream_time = timeit.timeit(lambda: roundtrip_file_stream(client, "in/test_data.parquet"), number=10)
-
-    huge_json_file_time = timeit.timeit(lambda: roundtrip_file(client, "in/huge_test_data.json"), number=1)
-    huge_csv_file_time  = timeit.timeit(lambda: roundtrip_file(client, "in/huge_test_data.csv"),  number=1)
-   
-    huge_csv_stream_time     = timeit.timeit(lambda: roundtrip_file_stream(client, "in/huge_test_data.csv"),     number=1)
-    huge_json_stream_time    = timeit.timeit(lambda: roundtrip_file_stream(client, "in/huge_test_data.json"),    number=1)
-    huge_parquet_stream_time = timeit.timeit(lambda: roundtrip_file_stream(client, "in/huge_test_data.parquet"), number=1)
- 
-    # print out the results 
-    print()
-    print("Operation                 Total Time Taken (s)")
-    print("----------                --------------------")
-    print("create client             {0}".format(end - start))
-    print("connection check  (x10)   {0}".format(connection_check_time))
-    print("\n>>> files <<<")
-    print("roundtrip csv     (x10)   {0}".format(csv_file_time))
-    print("roundtrip json    (x10)   {0}".format(json_file_time))
-    print("\n>>> streams <<<")
-    print("roundtrip csv     (x10)   {0}".format(csv_stream_time))
-    print("roundtrip json    (x10)   {0}".format(json_stream_time))
-    print("roundtrip parquet (x10)   {0}".format(parquet_stream_time))
-    print("\n>>> huge files <<<")
-    print("roundtrip json (x1)  {0}".format(huge_json_file_time))
-    print("roundtrip csv  (x1)  {0}".format(huge_csv_file_time))
-    print("\n>>> huge streams <<<")
-    print("roundtrip csv     (x1)   {0}".format(huge_csv_stream_time))
-    print("roundtrip json    (x1)   {0}".format(huge_json_stream_time))
-    print("roundtrip parquet (x1)   {0}".format(huge_parquet_stream_time))
-
-
-    # messing around w/ parquet
-    import pyarrow.parquet as pq
-    import pyarrow as pa
-
-    # write the parquet in-memory table
+def roundtrip_parquet_mem(client: plasma.PlasmaClient, table: pa.Table):
+    '''
+    Roundtrip an in-memory PyArrow Table to the Plasma store.
+    '''
     obj_id = random_obj_id()
-    pq_table = pq.read_table("in/test_data.parquet", memory_map=True)
 
     mock_sink = pa.MockOutputStream()
 
-    with pa.RecordBatchStreamWriter(mock_sink, pq_table.schema) as stream_writer:
-            stream_writer.write_table(pq_table)
+    with pa.RecordBatchStreamWriter(mock_sink, table.schema) as stream_writer:
+            stream_writer.write_table(table)
 
     data_size = mock_sink.size()
 
+    # write the parquet in-memory table
     buf = client.create(obj_id, data_size)
     stream = pa.FixedSizeBufferWriter(buf)
     
-    with pa.RecordBatchStreamWriter(stream, pq_table.schema) as stream_writer:
-            stream_writer.write_table(pq_table)
+    with pa.RecordBatchStreamWriter(stream, table.schema) as stream_writer:
+            stream_writer.write_table(table)
 
     client.seal(obj_id)
 
@@ -145,12 +110,89 @@ def main():
 
     reader = pa.RecordBatchStreamReader(buf)
 
-    pq_table = reader.read_all()
+    reader.read_all()
 
-    print()
-    print("Objects in the Store At End")
-    print("----------------------------")
-    print(len(client.list()))
+
+def time_and_output(out: Table, name: str, num: int, func):
+    '''
+    Time the specified function and output the results to the given Table. `name` 
+    and `num` specify what to put in the output table columns.
+    '''
+    exec_time = timeit.timeit(func, number=num)
+
+    avg_time = exec_time / num
+
+    out.add_row(name, str(num), str(exec_time), str(avg_time))
+
+
+def add_header_row(out: Table, title: str):
+    out.add_row("[cyan]>>> {0} <<<[/]".format(title), "", "", "")
+
+
+def main():
+    print("*** [green u]Starting the Plasma Benchmark[/] ***")
+    
+    # connect to the running plasma server
+    start = timeit.default_timer()
+    client = plasma.connect("/tmp/plasma")
+    end = timeit.default_timer()  
+
+    console = Console()
+    out = Table(Column("Operation", justify="center"), 
+        Column("Reps", justify="center"), 
+        "Total Time (s)", 
+        "Average Time (s)", 
+        show_header=True, 
+        header_style="cyan")
+    
+    with Progress("{task.description}", BarColumn(), "{task.percentage:>3.0f}%") as progress:
+        benchmark = progress.add_task("Progress", total=14)
+    
+        out.add_row("create client", str(1), str(end - start), str(end - start))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "connection check", 10, lambda: check_connection(client))
+        progress.update(benchmark, advance=1)
+
+        add_header_row(out, "FILES")
+        time_and_output(out, "csv", 10, lambda: roundtrip_file(client, "in/test_data.csv"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "json", 10, lambda: roundtrip_file(client, "in/test_data.json"))
+        progress.update(benchmark, advance=1)
+      
+        add_header_row(out, "STREAMS")
+        time_and_output(out, "csv", 10, lambda: roundtrip_file_stream(client, "in/test_data.csv"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "json", 10, lambda: roundtrip_file_stream(client, "in/test_data.json"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "parquet", 10, lambda: roundtrip_file_stream(client, "in/test_data.parquet"))
+        progress.update(benchmark, advance=1)
+
+        add_header_row(out, "HUGE FILES")
+        time_and_output(out, "csv", 1, lambda: roundtrip_file(client, "in/huge_test_data.json"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "json", 1, lambda: roundtrip_file(client, "in/huge_test_data.csv"))
+        progress.update(benchmark, advance=1)
+       
+        add_header_row(out, "HUGE STREAMS")
+        time_and_output(out, "csv", 1, lambda: roundtrip_file_stream(client, "in/huge_test_data.csv"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "json", 1, lambda: roundtrip_file_stream(client, "in/huge_test_data.json"))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "parquet", 1, lambda: roundtrip_file_stream(client, "in/huge_test_data.parquet"))
+        progress.update(benchmark, advance=1)
+
+        regular_pq_table = pq.read_table("in/test_data.parquet", memory_map=True)
+        huge_pq_table = pq.read_table("in/huge_test_data.parquet", memory_map=True)
+
+        add_header_row(out, "IN-MEMORY OBJS")
+        time_and_output(out, "reg. parquet", 10, lambda: roundtrip_parquet_mem(client, regular_pq_table))
+        progress.update(benchmark, advance=1)
+        time_and_output(out, "huge parquet", 10, lambda: roundtrip_parquet_mem(client, huge_pq_table))
+        progress.update(benchmark, advance=1)
+
+    console.print(out)
+
+    print("*** [green u]Ending the Plasma Benchmark[/] ***")
 
     # finally, disconnect the client
     client.disconnect()
